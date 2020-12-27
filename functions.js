d21a3cb023e1e3b7a10c28748898d6f57267b58b
MauroRiva98 <76596004+MauroRiva98@users.noreply.github.com>
2020-12-27 17:25:12 +0100

    Add files via upload

--- functions.js
+++ functions.js
@@ -65,7 +65,7 @@ function menu_click(option){
 							</div>\
 							<br><br><br>\
 							<div id=\"div_rule_assegnee_1\" name=\"div_rule_assegnee\" class=\"div_rule_assegnee\">\
-								<label class=\"unselectable\" for=\"assegnee\">Assegnee:</label><br>\
+								<label class=\"unselectable\" for=\"assegnee\">Assignee:</label><br>\
 								<input type=\"text\" id=\"assegnee_1\" name=\"assegnee\" class=\"input_create add_prefix\">\
 							</div>\
 							<label class=\"unselectable\" for=\"target\">Target:</label><br>"
@@ -99,8 +99,19 @@ function menu_click(option){
 						</div>\
 					  </div>\
 					  <br>\
-					  <div id=\"div_add_rule\" onclick=\"add_new_rule()\">\
+					  <div id=\"div_add_rule\" class=\"scroll_bottom\" onclick=\"add_new_rule()\">\
 						<label class=\"unselectable\" id=\"label_add_rule\">Add new rule</label>\
+					  </div>\
+					  <div id=\"div_show\" name=\"div_show\" class=\"scroll_bottom\" onclick=\"show_policy()\">\
+						<label class=\"unselectable\" id=\"label_div_show\">Show policy</label>\
+					  </div>\
+					  <div id=\"div_download\">\
+						<label class=\"unselectable\">Nome file:</label><br>\
+						<input type=\"text\" id=\"file_name\" name=\"file_name\">\
+						<label class=\"unselectable\">.jsonld</label>\
+						<div id=\"div_download_button\" onclick=\"start_download()\">\
+							<label class=\"unselectable\" id=\"label_div_download_button\">Download</label>\
+						</div>\
 					  </div>\
 					</form>\
 				</div>");
@@ -114,12 +125,21 @@ function menu_click(option){
 	}
 }
 
+function show_alert(text_alert){    	
+    $("#div_main").append("<div class=\"alert unselectable\"> <span class=\"closebtn\" onclick=\"close_alert(this.parentElement)\">&times;</span>" + text_alert +" </div>");
+    }
+
+function close_alert(element_div){
+	$(element_div).remove();
+}
+
 function plus_click(type,number,rule){
 	switch(type) {
 		case "context":
 			$("#div_buttons_context_"+$('div[name ="div_context"]').length).hide();
 			$(".scroll_context").css("margin-top",(($('div[name ="div_context"]').length)*35)+"px");
-			$("#div_add_rule").css("margin-top",(parseInt($("#div_add_rule").css("margin-top"),10) + 35) + "px");
+			$(".scroll_bottom").css("margin-top",(parseInt($("#div_add_rule").css("margin-top"),10) + 35) + "px");
+			$("#div_download").css("margin-top",(parseInt($("#div_download").css("margin-top"),10) + 35) + "px");
 			$("#div_context_ext").append("<div id=\"div_context_"+($('div[name ="div_context"]').length + 1)+"\" name=\"div_context\" class=\"div_context\" style=\"margin-top:"+(($('div[name ="div_context"]').length)*35)+"\px\">\
 							<input type=\"text\" id=\"context_"+($('div[name ="div_context"]').length + 1)+"\" name=\"context\" class=\"input_create\">\
 							<div id=\"div_buttons_context_"+($('div[name ="div_context"]').length + 1)+"\" class=\"div_buttons_context\">\
@@ -142,7 +162,8 @@ function plus_click(type,number,rule){
 			$("#div_rule_action_"+rule).css("top",(($(string_id).length)*41)+"px");
 			$("#div_rule_purpose_"+rule).css("top",(($(string_id).length)*41)+"px");
 			$("#div_rule_"+rule).css("height",($("#div_rule_"+rule).height() +41)+"px");
-			$("#div_add_rule").css("margin-top",(parseInt($("#div_add_rule").css("margin-top"),10) + 41) + "px");
+			$(".scroll_bottom").css("margin-top",(parseInt($("#div_add_rule").css("margin-top"),10) + 41) + "px");
+			$("#div_download").css("margin-top",(parseInt($("#div_download").css("margin-top"),10) + 41) + "px");
 			for (var i = rule_number; i > rule; i--) {				
 				$("#div_rule_"+i).css("margin-top",(parseInt($("#div_rule_"+i).css("margin-top"),10) + 41) + "px");
 			}
@@ -172,7 +193,8 @@ function minus_click(type,number,rule){
 				$("#div_context_"+$('div[name ="div_context"]').length).remove();
 				$("#div_buttons_context_"+($('div[name ="div_context"]').length)).show();
 				$(".scroll_context").css("margin-top",(($('div[name ="div_context"]').length - 1)*35)+"px");
-				$("#div_add_rule").css("margin-top",(parseInt($("#div_add_rule").css("margin-top"),10) - 35) + "px");
+				$(".scroll_bottom").css("margin-top",(parseInt($("#div_add_rule").css("margin-top"),10) - 35) + "px");
+				$("#div_download").css("margin-top",(parseInt($("#div_download").css("margin-top"),10) - 35) + "px");
 			}
 		break;
 		case "target":
@@ -183,7 +205,8 @@ function minus_click(type,number,rule){
 				$("#div_rule_action_"+rule).css("top",(($(string_id).length - 1)*41)+"px");
 				$("#div_rule_purpose_"+rule).css("top",(($(string_id).length - 1)*41)+"px");
 				$("#div_rule_"+rule).css("height",($("#div_rule_"+rule).height() -41)+"px");
-				$("#div_add_rule").css("margin-top",(parseInt($("#div_add_rule").css("margin-top"),10) - 41) + "px");
+				$(".scroll_bottom").css("margin-top",(parseInt($("#div_add_rule").css("margin-top"),10) - 41) + "px");
+				$("#div_download").css("margin-top",(parseInt($("#div_download").css("margin-top"),10) - 41) + "px");
 				for (var i = rule_number; i > rule; i--) {				
 					$("#div_rule_"+i).css("margin-top",(parseInt($("#div_rule_"+i).css("margin-top"),10) - 41) + "px");
 				}
@@ -256,14 +279,16 @@ function add_new_rule(){
 							<br>\
 						</div>");	
 	$("#div_rule_"+rule_number).css("margin-top",((len * 385) + len_target * 41)+"px");
-	$("#div_add_rule").css("margin-top",((len * 385) + (len_target * 41) + ((len_context - 1) * 35))+"px");
+	$(".scroll_bottom").css("margin-top",((len * 385) + (len_target * 41) + ((len_context - 1) * 35))+"px");
+	$("#div_download").css("margin-top",(parseInt($("#div_add_rule").css("margin-top"),10)) + "px");
 }
 
 function delete_rule_click(rule){
 	if($('div[name ="div_rule"]').length > 1){
 		var height = parseInt($("#div_rule_"+rule).css("height"),10);
 		$("#div_rule_"+rule).remove();
-		$("#div_add_rule").css("margin-top",(parseInt($("#div_add_rule").css("margin-top"),10) - (height + 47)) + "px");
+		$(".scroll_bottom").css("margin-top",(parseInt($("#div_add_rule").css("margin-top"),10) - (height + 47)) + "px");
+		$("#div_download").css("margin-top",(parseInt($("#div_add_rule").css("margin-top"),10)) + "px");
 		for (var i = rule_number; i > rule; i--) {				
 			$("#div_rule_"+i).css("margin-top",(parseInt($("#div_rule_"+i).css("margin-top"),10) - (height + 47)) + "px");
 		}
@@ -273,4 +298,138 @@ function delete_rule_click(rule){
 function prefix_change(){
 	var prefix = $("#prefix").val();
 	$(".add_prefix").val(prefix);
+}
+
+function check_form(){
+	var valid = true;
+	if($("#uid_ext").val() == "" || $("#type_ext").val() == ""){
+		valid = false;
+		return valid;
+	}
+	for (var i = $('div[name ="div_context"]').length; i > 0; i--) {
+		if($("#context_"+i).val() == ""){
+			valid = false;
+			return valid;
+		}
+	}
+	for (var i = $('div[name ="div_rule"]').length; i > 0; i--) {
+		if($("#uid_"+i).val() == "" || $("#assegnee_"+i).val() == "" || $("#action_"+i).val() == "" || $("#purpose_"+i).val() == ""){
+			valid = false;
+			return valid;
+		}	
+		for (var j = $("div[name =\"div_rule_target_"+i+"\"]").length; j > 0; j--) {
+			if($("#target_"+i+"_"+j).val() == ""){
+				valid = false;
+				return valid;
+			}
+		}
+	}
+	return valid;
+}
+
+function get_policy(){
+	var rules = [];
+	for (var i = 1; i <= $('div[name ="div_rule"]').length; i++) {
+		var target = [];
+		for(var j = 1; j <= $("div[name =\"div_rule_target_"+i+"\"]").length; j++){
+			var t = $("#target_"+i+"_"+j).val();
+			target.push(t);
+		}
+		var r = {
+			"type": $("#rule_type_"+i).val(),
+			"uid": $("#uid_"+i).val(),
+			"assignee": $("#assegnee_"+i).val(),
+			"target": target,
+			"action": $("#action_"+i).val(),
+			"purpose": $("#purpose_"+i).val()
+		}
+		rules.push(r);
+	}
+	var context = [];
+	for(var k = 1; k <= $("div[name =\"div_context\"]").length; k++){
+		var c = $("#context_"+k).val();
+		context.push(c);
+	}
+	var permission = [];
+	var prohibition = [];
+	var obligation = [];
+	
+	for(var i = 0; i < rules.length; i++) {
+		switch(rules[i]["type"]) {
+			case "permission":
+				var per = {
+					"uid": rules[i]["uid"],
+					"assignee": rules[i]["assignee"],
+					"target": rules[i]["target"],
+					"action": rules[i]["action"],
+					"purpose": rules[i]["purpose"]
+				};
+				permission.push(per);
+			break;
+			case "prohibition":
+				var proh = {
+					"uid": rules[i]["uid"],
+					"assignee": rules[i]["assignee"],
+					"target": rules[i]["target"],
+					"action": rules[i]["action"],
+					"purpose": rules[i]["purpose"]
+				};
+				prohibition.push(proh);
+			break;
+			case "obligation":
+				var obli = {
+					"uid": rules[i]["uid"],
+					"assignee": rules[i]["assignee"],
+					"target": rules[i]["target"],
+					"action": rules[i]["action"],
+					"purpose": rules[i]["purpose"]
+				};
+				obligation.push(obli);
+			break;
+		}
+	}
+	var policy = {
+		"@context": context,
+		"@type": $("#type_ext").val(),
+		"uid": $("#uid_ext").val(),
+		"permission": permission,
+		"prohibition": prohibition,
+		"obligation": obligation
+	};
+	if(permission.length == 0)
+		delete policy.permission;
+	if(prohibition.length == 0)
+		delete policy.prohibition;
+	if(obligation.length == 0)
+		delete policy.obligation;
+	return policy;
+}
+
+function download(filename, text) {
+	var element = document.createElement('a');
+	element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+	element.setAttribute('download', filename);
+
+	element.style.display = 'none';
+	document.body.appendChild(element);
+
+	element.click();
+
+	document.body.removeChild(element);
+}		
+
+function start_download(){
+	if($("#file_name").val() == ""){
+		show_alert("ERROR: Insert a file name!");
+	}
+	else{
+		if(!check_form())
+			show_alert("ERROR: Empty text input!");
+		else{
+			var policy = get_policy();
+			var myJSON = JSON.stringify(policy, null, 2);
+			var file_name = ($("#file_name").val() + ".jsonld");
+			download(file_name, myJSON);
+		}
+	}
 }
